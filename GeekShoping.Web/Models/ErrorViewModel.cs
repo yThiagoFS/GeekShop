b2a85b0e1f4cddb658cbe503c82ceff624f95e3e
Thiago Ferreira <thi.ferreira.silva03@gmail.com>
2023-04-02 14:33:35 -0300

    First commit

---
+++ GeekShoping.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GeekShoping.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ GeekShopping.ProductAPI/Model/Context/SQLContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class SQLContext : DbContext
+    {
+        public SQLContext() {}
+
+        public SQLContext(DbContextOptions<SQLContext> opts) : base(opts) {}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
